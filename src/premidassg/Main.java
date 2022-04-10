4b90721e8139c0ee6ecef6b36671cc0336cd5d0d
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2022-04-10 19:11:54 +0700

    Second Revision + Delete Functions

--- src/premidassg/Main.java
+++ src/premidassg/Main.java
@@ -30,7 +30,7 @@ public class Main {
 
 				switch (inp) {
 					case 1:
-						minyak.add(newMinyak());
+						newMinyak(minyak);
 						System.out.println("Press Enter to continue...");
 						sc.nextLine();
 						break;
@@ -62,12 +62,30 @@ public class Main {
 						sc.nextLine();
 						break;
 					case 5:
+						if(minyak.size() == 0) {
+							System.out.println("Tidak ada data minyak");
+						}else {
+							deleteMinyak(minyak);
+						}
+						System.out.println("Press Enter to continue...");
+						sc.nextLine();
+						break;
+					case 6:
+						if(minyak.size() == 0) {
+							System.out.println("Tidak ada data transaksi");
+						}else {
+							deleteTransaction(transaction);
+						}
+						System.out.println("Press Enter to continue...");
+						sc.nextLine();
+						break;
+					case 7:
 						isRun = !isRun;
 					default:
 						
 				}
 
-			} while (inp < 1 && inp > 5);
+			} while (inp < 1 && inp > 7);
 
 		} while (isRun);
 
@@ -82,13 +100,15 @@ public class Main {
 		System.out.println("2. Tambah Transaksi");
 		System.out.println("3. View Minyak");
 		System.out.println("4. View Transaksi");
-		System.out.println("5. Exit");
+		System.out.println("5. Delete Minyak");
+		System.out.println("6. Delete Transaksi");
+		System.out.println("7. Exit");
 		System.out.println("===============");
 		System.out.print("Choice >> ");
 
 	}
 
-	public Minyak newMinyak() {
+	public void newMinyak(ArrayList<Minyak> minyak) {
 
 		Scanner sc = new Scanner(System.in);
 		String ID = "";
@@ -97,6 +117,9 @@ public class Main {
 		int quantity = 0;
 		int price = 0;
 		double rand1; double rand2; double rand3;
+		boolean check = false;
+		int index = 0;
+		int stokMinyak = 0;
 
 		rand1 = Math.random() * (9 - 0) + 0;
 		rand2 = Math.random() * (9 - 0) + 0;
@@ -105,39 +128,81 @@ public class Main {
 
 		System.out.print("Masukkan nama minyak: ");
 		name = sc.next();
-		System.out.print("Masukkan brand minyak: ");
-		brand = sc.next();
+
+		if (!minyak.isEmpty()) {
+
+			for (Minyak minyak2 : minyak) {
+			
+				if(name.equals(minyak2.getName()) == true) {
+	
+					check = true;
+					break;
+	
+				}
+	
+				index++;
+	
+			}
+
+		}
+
+		if (check == true) {
+			
+			do {
+				
+				System.out.print("Masukkan jumlah minyak: ");
+				try {
+					quantity = sc.nextInt();
+				} catch (Exception e) {
+					//TODO: handle exception
+					System.out.println("Jumlah minyak harus diatas 0!");
+					quantity = -1;
+				}
+				sc.nextLine();
+
+			} while (quantity < 1);
+
+			stokMinyak = (int)minyak.get(index).getQuantity() + quantity;
+			minyak.get(index).setQuantity(stokMinyak);
+
+		} else {
+
+			System.out.print("Masukkan brand minyak: ");
+			brand = sc.next();
+			
+			do {
+				
+				System.out.print("Masukkan jumlah minyak: ");
+				try {
+					quantity = sc.nextInt();
+				} catch (Exception e) {
+					//TODO: handle exception
+					System.out.println("Jumlah minyak harus diatas 0!");
+					quantity = -1;
+				}
+				sc.nextLine();
+
+			} while (quantity < 1);
+
+			do {
+				
+				System.out.print("Masukkan harga minyak: ");
+				try {
+					price = sc.nextInt();
+				} catch (Exception e) {
+					//TODO: handle exception
+					System.out.println("Harga harus diatas 0!");
+					price = -1;
+				}
+				sc.nextLine();
+
+			} while (price < 1);
+
+			Minyak newData = new Minyak(ID, name, brand, quantity, price);
+			minyak.add(newData);
+
+		}
 		
-		do {
-			
-			System.out.print("Masukkan jumlah minyak: ");
-			try {
-				quantity = sc.nextInt();
-			} catch (Exception e) {
-				//TODO: handle exception
-				System.out.println("Jumlah minyak harus diatas 0!");
-				quantity = -1;
-			}
-			sc.nextLine();
-
-		} while (quantity < 1);
-
-		do {
-			
-			System.out.print("Masukkan harga minyak: ");
-			try {
-				price = sc.nextInt();
-			} catch (Exception e) {
-				//TODO: handle exception
-				System.out.println("Harga harus diatas 0!");
-				price = -1;
-			}
-			sc.nextLine();
-
-		} while (price < 1);
-
-		return new Minyak(ID, name, brand, quantity, price);
-
 	}
 
 	public void showMinyak(ArrayList<Minyak> minyak) {
@@ -168,6 +233,7 @@ public class Main {
 		TransactionDetail transactionDetail[] = new TransactionDetail[100];
 		int inp = 1;
 		int i = 0;
+		int stokMinyak = 0;
 
 		rand1 = Math.random() * (9 - 0) + 0;
 		rand2 = Math.random() * (9 - 0) + 0;
@@ -223,6 +289,8 @@ public class Main {
 					} while (quantity < 0 && quantity > minyak.get(index).getQuantity());
 
 					price = (int)minyak.get(index).getPrice() * quantity;
+					stokMinyak = (int)minyak.get(index).getQuantity() - quantity;
+					minyak.get(index).setQuantity(stokMinyak);
 					totalHarga = totalHarga + price;
 
 					transactionDetail[i] = new TransactionDetail(minyakName, quantity, price);
@@ -274,6 +342,54 @@ public class Main {
 
 			System.out.printf(" Rp %d | %s \n", transaction2.getTotalHarga(), transaction2.getDate());
 
+		}
+
+	}
+
+	private void deleteMinyak(ArrayList<Minyak> minyak) {
+
+		Scanner sc = new Scanner(System.in);
+		String ID;
+		int index = 0;
+
+		showMinyak(minyak);
+		System.out.print("Masukkan ID Minyak yang ingin dihapus: ");
+		ID = sc.next();
+
+		for (Minyak minyak2: minyak) {
+			
+			if(ID.equals(minyak2.getID())) {
+
+				minyak.remove(index);
+				break;
+
+			}
+
+			index++;
+		}
+
+	}
+
+	private void deleteTransaction(ArrayList<Transaction> transaction) {
+
+		Scanner sc = new Scanner(System.in);
+		String ID;
+		int index = 0;
+
+		showTransaction(transaction);
+		System.out.print("Masukkan ID Transaksi yang ingin dihapus: ");
+		ID = sc.nextLine();
+
+		for (Transaction transaction2 : transaction) {
+			
+			if(ID.equals(transaction2.getID())) {
+
+				transaction.remove(index);
+				break;
+
+			}
+
+			index++;
 		}
 
 	}
